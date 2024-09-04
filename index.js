f3e4b933b72567652136b6172b5dd2bbb3e5e952
Miki <muneshmyke@gmail.com>
2024-09-04 20:11:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -257,6 +257,16 @@ app.get('/transactions', async (req, res) => {
   }
 });
 
+app.get('/referals', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM referals');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
 app.get('/rewards', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM rewards');
